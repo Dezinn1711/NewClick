366b1ed227a5b57c4380048982e9b756ec198001
Dezinn1711 <andreluizrosa4@gmail.com>
2025-10-24 22:35:06 -0300

    Iniciando o Projeto e Fazendo as Models

---
+++ NewClick.API/Models/Categoria.cs
@@ -0,0 +1,20 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace NewClick.API.Models;
+
+    public class Categoria
+{
+    [Key]
+    public int CategoriaId { get; set; }
+
+    [StringLength(50)]
+    [Required(ErrorMessage = "O nome é obrigatório")]
+    public string Nome { get; set; }
+
+    [StringLength(300)]
+    public string Foto { get; set; }
+
+    [StringLength(26)]
+    public string Cor { get; set; } = "rgba(0,0,0,1)";
+}
---
+++ NewClick.API/Models/Produto.cs
@@ -0,0 +1,42 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace NewClick.API.Models;
+
+public class Produto
+{
+    [Key]
+    public int Id { get; set; }
+
+    [ForeignKey("CategoriaId")]
+    public Categoria Categoria { get; set; }
+
+    [StringLength(100)]
+    [Required(ErrorMessage = "O nome é obrigatório")]
+    public string Nome { get; set; }
+
+    [StringLength(3000)]
+    [Display(Name = "Descrição")]
+    public string Descricao { get; set; }
+
+
+    [Required(ErrorMessage = "A quantidade é obrigatório")]
+    [Display(Name = "Quantidade")]
+    public int Qtde { get; set; }
+
+    [Column(TypeName = "decimal(12,2)")]
+    [Display(Name = "Valor de Custo")]
+    [Required(ErrorMessage = "O valor de custo é obrigatório")]
+    public decimal ValorCusto { get; set; }
+
+    [Column(TypeName = "decimal(12,2)")]
+    [Display(Name = "Valor de Venda")]
+    [Required(ErrorMessage = "O valor de venda é obrigatório")]
+    public decimal ValorVenda { get; set; }
+
+    [Required(ErrorMessage = "O destaque é obrigatório")]
+    public bool Destaque { get; set; } = false;
+
+    [StringLength(300)]
+    public string Foto { get; set; }
+}
